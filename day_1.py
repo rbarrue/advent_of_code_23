1a6ffce78a033a4efc6a66082bb362b1165af98b
Ricardo Barrué <ricardo.barrue@cern.ch>
2023-12-03 23:21:21 +0000

    Another brave (failed) attempt at day 1 part 2

--- day_1.py
+++ day_1.py
@@ -35,6 +35,25 @@ def find_all(sub,str):
     
     return list_return
 
+def decode(num):
+
+    dict_alphabetic_digits={
+        'one':1,
+        'two':2,
+        'three':3,
+        'four':4,
+        'five':5,
+        'six':6,
+        'seven':7,
+        'eight':8,
+        'nine':9
+    }
+
+    if num in dict_alphabetic_digits:
+        return dict_alphabetic_digits[num]
+    else:
+        return int(num)
+            
 # not working atm
 def part2_v1():
 
@@ -75,11 +94,6 @@ def part2_v1():
         final_list=list(zip(list_digit_indices_total,list_digits_total))
         final_list.sort(key=lambda item: item[0])
         
-        def decode(num):
-            if num in dict_alphabetic_digits:
-                return dict_alphabetic_digits[num]
-            else:
-                return int(num)
 
         tens=decode(final_list[0][1])
         units=decode(final_list[-1][1])
@@ -93,5 +107,40 @@ def part2_v1():
 
     return num
 
+# trying again after learning about the existence of rfind
+def part2_v2():
+
+    first_digit=''
+    last_digit=''
+
+    list_digits=['one','two','three','four','five','six','seven','eight','nine']
+    list_digits+=map(str,range(1,10))
+    
+    sum_calibration_values=0
+
+    for line in f.readlines():
+        i_start_first_digit=len(line)
+        i_start_last_digit=0
+        for digit in list_digits:
+            i_start_digit=line.find(digit)
+            i_last_digit=line.rfind(digit)
+            
+            if i_start_digit != -1:
+                if i_start_digit < i_start_first_digit:
+                    i_start_first_digit = i_start_digit
+                    first_digit=digit
+            
+            if i_last_digit != -1:
+                if i_last_digit > i_start_last_digit:
+                    i_start_last_digit = i_last_digit
+                    last_digit=digit
+        
+        if i_start_first_digit==i_start_last_digit:
+            sum_calibration_values+=decode(first_digit)
+        else:
+            sum_calibration_values+=(10*decode(first_digit)+decode(last_digit))
+    
+    return sum_calibration_values
+
 if __name__=='__main__':
-    print(part2_v1())
+    print(part2_v2())
